9e55a83f744ed6eff3127643f3d8a51334fcac31
hacker228-kz <math7@inbox.ru>
2024-11-20 12:41:10 +0600

    solution

---
+++ console.js
@@ -0,0 +1,8 @@
+function evenOrOdd(number) {
+    if(number % 2 === 0) { 
+      return "Even"
+    } else { 
+      return 'Odd'
+    }
+  }
+  // ссылка на задачу https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
